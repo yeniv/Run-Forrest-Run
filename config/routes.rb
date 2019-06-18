187ba7e8bfecf5c1f17c30bfd4e66ccea0c27368
Tristan Viney <yenivyeniv@gmail.com>
2019-06-18 13:17:45 +0200

    sidekiq routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  # require 'sidekiq/web'
-  # authenticate :user, lambda { |u| u.admin } do
-  #   mount Sidekiq::Web => '/sidekiq'
-  # end
+  require 'sidekiq/web'
+  authenticate :user, lambda { |u| u.admin } do
+    mount Sidekiq::Web => '/sidekiq'
+  end
 end
